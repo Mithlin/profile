c2a6a695506a1f27f5b67f8010f3bff82d6decef
lawliet <lawliet@toybox.(none)>
2013-08-11 13:42:26 +0800

    delete unused file

--- linux/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mv /home/`whoami`/.vimrc /home/`whoami`/.vimrc~
-mv /home/`whoami`/.bashrc /home/`whoami`/.bashrc~
-
-cp ./vimrc.txt /home/`whoami`/.vimrc
-cp ./bashrc.txt /home/`whoami`/.bashrc
-
